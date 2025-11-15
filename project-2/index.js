90e07ac94a125b1af5a2e6ac439f72e7406ac819
SakshiDeep5 <deepsakshi07@gmail.com>
2025-11-15 10:45:43 +0530

    updated project-2 files

--- project-2/index.js
+++ project-2/index.js
@@ -11,10 +11,39 @@ const textArray=[
     "Software Engineer",
     "Project Manager",
     "Web Developer",
-    "Category Lead"
+    "Category Lead",
 ];
 
 const typingDelay=50;
 const erasingDelay=50;
 const newTextDelay=200;
-const 
+let textArrayIndex=0;
+let charIndex=0;
+
+function type(){
+    if(charIndex < textArray[textArrayIndex].length){
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type,typingDelay);
+    } else{
+        setTimeout(erase,newTextDelay);
+    }
+}
+
+function erase(){
+    if(charIndex > 0){
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(
+            0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }else{
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    //on DOM load initiate the effect
+    if(textArray.length) setTimeout(type,newTextDelay + 250);
+});
